83f828f53a533d6cc0b29d9394cb87f8cec986a5
Patrick LIM <patrick.lim012@gmail.com>
2018-03-22 15:54:40 +0100

    update default theme

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { createMuiTheme } from 'material-ui';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['open sans', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+  },
+});
+
+export default theme;
