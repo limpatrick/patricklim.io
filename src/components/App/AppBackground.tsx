3d37d9d5d73c8ced024687e35f3c80f8dca3bb47
limpatrick <contact@patricklim.fr>
2018-03-22 23:52:17 +0100

    update background

--- src/components/App/AppBackground.tsx
+++ src/components/App/AppBackground.tsx
@@ -6,7 +6,9 @@ import { WithStyles, withStyles } from 'material-ui/styles';
 interface AppBackgroundProps {}
 
 const AppBackground: React.SFC<AppBackgroundProps & WithStyles<AppBackgroundStyles>> = ({ classes }) => (
-  <div className={classes.background} />
+  <div className={classes.container}>
+    <div className={classes.background} />
+  </div>
 );
 
 export default withStyles(appBackgroundStyles)(AppBackground);
--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,9 @@
 import { createMuiTheme } from 'material-ui';
 
 const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
   typography: {
     fontFamily: ['open sans', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
   },
