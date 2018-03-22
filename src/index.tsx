d211c955eeef74a9a31eac02808fa64ac2ff21fa
limpatrick <contact@patricklim.fr>
2018-03-22 15:54:40 +0100

    update default theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /// <reference path="utils/modules.d.ts" />
 
 import 'typeface-roboto';
+import 'typeface-open-sans';
 
 import * as React from 'react';
 
@@ -8,13 +9,21 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from 'components/App/App';
 import CssBaseline from 'material-ui/CssBaseline';
+import { MuiThemeProvider } from 'material-ui/styles';
 import { render } from 'react-dom';
+import theme from './theme';
 
 render(
   <div>
     <CssBaseline />
     <BrowserRouter>
-      <Route component={App} />
+      <Route
+        render={() => (
+          <MuiThemeProvider theme={theme}>
+            <App />
+          </MuiThemeProvider>
+        )}
+      />
     </BrowserRouter>
   </div>,
   document.getElementById('root')
