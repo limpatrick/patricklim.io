70ef58e6399051c141dfe6b5fc08abe8c54f0493
Patrick LIM <patrick.lim012@gmail.com>
2018-03-24 19:19:46 +0100

    update logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,19 +3,23 @@ import * as React from 'react';
 import { WithStyles, withStyles } from 'material-ui/styles';
 import styles, { LogoStyles } from './styles';
 
+import Grow from 'material-ui/transitions/Grow';
+
 interface LogoProps {
   className?: string;
 }
 
-const App: React.SFC<LogoProps & WithStyles<LogoStyles>> = ({ classes, className }) => {
+const Logo: React.SFC<LogoProps & WithStyles<LogoStyles>> = ({ classes, className }) => {
   const logoClassName = className ? `${classes.logo} ${className}` : classes.logo;
 
   return (
-    <div className={logoClassName}>
-      <span className={classes.letter}>P</span>
-      <span className={classes.letter}>L</span>
-    </div>
+    <Grow in timeout={3000}>
+      <div className={logoClassName}>
+        <span className={classes.letter}>P</span>
+        <span className={classes.letter}>L</span>
+      </div>
+    </Grow>
   );
 };
 
-export default withStyles(styles)(App);
+export default withStyles(styles)(Logo);
