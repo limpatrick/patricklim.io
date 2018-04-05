9fa97f38c545606fd930ab9bbe969844a1156b8b
Patrick LIM <patrick.lim012@gmail.com>
2018-04-05 03:54:43 +0200

    toggle timeline navigation

--- src/components/HorizontalTimeline/Navigation/index.tsx
+++ src/components/HorizontalTimeline/Navigation/index.tsx
@@ -7,16 +7,22 @@ import IconButton from 'material-ui/IconButton';
 import { NavigationStyles } from './styles';
 
 interface NavigationProps {
-  type: NavigationType;
+  disabled: boolean;
   onClick: (type: NavigationType) => void;
+  type: NavigationType;
 }
 
-const Navigation: React.SFC<NavigationProps & WithStyles<NavigationStyles>> = ({ classes, type, onClick }) => {
+const Navigation: React.SFC<NavigationProps & WithStyles<NavigationStyles>> = ({
+  classes,
+  disabled,
+  onClick,
+  type,
+}) => {
   const Chevron = type === 'previous' ? FaChevronLeft : FaChevronRight;
 
   return (
     <div className={classes.container}>
-      <IconButton aria-label={type} onClick={() => onClick(type)}>
+      <IconButton aria-label={type} onClick={() => onClick(type)} disabled={disabled}>
         <Chevron />
       </IconButton>
     </div>
