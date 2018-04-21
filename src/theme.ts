a99d92ce2472461b423285db83a6e9b9953d9fd2
limpatrick <contact@patricklim.fr>
2018-04-21 23:22:09 +0200

    disable user select on typography component

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,13 @@ import { createMuiTheme } from 'material-ui';
 export const fontFamily = ['open sans', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'];
 
 const theme = createMuiTheme({
+  overrides: {
+    MuiTypography: {
+      root: {
+        userSelect: 'none',
+      },
+    },
+  },
   palette: {
     type: 'dark',
   },
