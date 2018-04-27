e45a8c1d1aa26b1b46a6e7221728f05368c5b253
limpatrick <contact@patricklim.fr>
2018-04-27 17:34:29 +0200

    update home view component

--- src/components/views/Home/styles.ts
+++ src/components/views/Home/styles.ts
@@ -1,14 +1,20 @@
 import { StyleRules } from 'material-ui/styles';
 
-export type HomeStyles = 'container' | 'text';
+export type HomeStyles = 'container' | 'title' | 'icon' | 'marginTop';
 export const HomeStyles: StyleRules<HomeStyles> = {
   container: {
     height: '100%',
     minHeight: 'fit-content',
   },
-  text: {
+  title: {
     color: 'rgba(255, 255, 255, 0.7)',
     fontWeight: 100,
-    margin: 0,
+  },
+  icon: {
+    marginRight: 4,
+    fontSize: 20,
+  },
+  marginTop: {
+    marginTop: 75,
   },
 };
