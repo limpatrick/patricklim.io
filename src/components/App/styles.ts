50335230da1842187700a561be416c345ebcb1c4
limpatrick <contact@patricklim.fr>
2018-05-04 16:18:09 +0200

    update app style

--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -4,54 +4,45 @@ import { StyleRulesCallback } from 'material-ui/styles';
 
 export const scrollbarsContentClass = 'scrollbarsContent';
 
-export type AppStyles = 'wrapper' | 'content' | 'scrollbars' | 'minHeight';
+export type AppStyles = 'container' | 'scrollbars' | 'content';
 export const AppStyles: StyleRulesCallback<AppStyles> = (theme) => {
   const headerHeight = 64;
   const xsHeaderHeight = 48;
-  const calcHeight = `calc(100vh - ${headerHeight}px)`;
-  const calcXsHeight = `calc(100vh - ${xsHeaderHeight}px)`;
+  const footerHeight = 48;
 
   return {
-    wrapper: {
+    container: {
       position: 'relative',
       top: 0,
       height: '100vh',
       overflow: 'hidden',
       background: primaryBgColor,
     },
-    content: {
-      height: '100%',
-      width: '100%',
-    },
+    content: {},
     scrollbars: {
-      marginTop: headerHeight,
-      height: `${calcHeight} !important`,
-      [theme.breakpoints.down('xs')]: {
-        marginTop: xsHeaderHeight,
-        height: `${calcXsHeight} !important`,
+      '& > div:first-child': {
+        marginTop: headerHeight,
+        [theme.breakpoints.down('xs')]: {
+          marginTop: xsHeaderHeight,
+        },
       },
-      '& > div > div': {
+      '& $content': {
+        minHeight: `calc(100vh - ${headerHeight}px)`,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
-        '& > *:first-child': {
-          background: altColor,
-        },
-        '&$minHeight': {
-          minHeight: calcHeight,
-        },
-      },
-      [`& .${scrollbarsContentClass}`]: {
-        minHeight: 'calc(100vh - 112px)',
         [theme.breakpoints.down('xs')]: {
-          minHeight: 'calc(100vh - 96px)',
+          minHeight: `calc(100vh - ${xsHeaderHeight}px)`,
         },
-      },
-    },
-    minHeight: {
-      overflow: 'hidden',
-      [theme.breakpoints.down(400)]: {
-        overflow: 'visible',
+        '& > div:first-child': {
+          background: altColor,
+          [`& .${scrollbarsContentClass}`]: {
+            minHeight: `calc(100vh - ${headerHeight + footerHeight}px)`,
+            [theme.breakpoints.down('xs')]: {
+              minHeight: `calc(100vh - ${xsHeaderHeight + footerHeight}px)`,
+            },
+          },
+        },
       },
     },
   };
