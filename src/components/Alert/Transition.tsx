27fb0e6313d8ca9b0ba4bb94fa67ad5310a66f0d
Patrick Lim <patrick.lim012@gmail.com>
2020-05-11 23:30:55 +0200

    set up v2 with gatsby

--- src/components/Alert/Transition.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-import Slide from 'material-ui/transitions/Slide';
-
-const Transition: React.SFC = (props) => <Slide direction="down" {...props} />;
-
-export default Transition;
---
+++ src/components/layout.tsx
@@ -0,0 +1,46 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import './layout.css';
+import { graphql, useStaticQuery } from 'gatsby';
+import React from 'react';
+import Header from './header';
+
+type Props = { children: React.ReactNode };
+
+const Layout = ({ children }: Props) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}>
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
