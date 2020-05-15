4b0aed8ce4589d843f7e6f3533afc3c8184fa69d
limpatrick <contact@patricklim.fr>
2020-05-15 22:15:40 +0200

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Patrick Lim - Full stack developer`,
+    description: `Full stack developer on React and Node.js`,
+    author: `Patrick Lim`,
   },
   plugins: [
     `top-layout`,
