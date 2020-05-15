2f7a91d309a66978db100736fb75daabfec8dc2f
limpatrick <contact@patricklim.fr>
2020-05-15 18:27:18 +0200

    update export

--- src/components/skill/reducer.ts
+++ src/components/skill/reducer.ts
@@ -1,6 +1,6 @@
 import { Action, DISPLAY_ACTIVE, DISPLAY_DEFAULT, State } from './types';
 
-export function reducer(state: State, { type, payload }: Action): State {
+export default function reducer(state: State, { type, payload }: Action): State {
   switch (type) {
     case DISPLAY_DEFAULT:
     case DISPLAY_ACTIVE:
