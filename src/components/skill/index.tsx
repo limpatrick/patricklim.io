9725c0c66723bb80c7de37410c6ad37495b7edb8
Patrick Lim <patrick.lim012@gmail.com>
2020-05-18 23:46:11 +0200

    replace useReducer by useState

--- src/components/skill/index.tsx
+++ src/components/skill/index.tsx
@@ -1,17 +1,15 @@
-import React, { useReducer } from 'react';
+import React, { useState } from 'react';
 import { Skill as SkillType } from '~/data/skills';
 import { Icon } from '@iconify/react';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { displayActive, displayDefault } from './actions';
-import reducer from './reducer';
 import useStyles from './styles';
 
 type Props = SkillType;
 
 const Skill = ({ name, default: dflt, active }: Props) => {
-  const [{ icon, height, width }, dispatch] = useReducer(reducer, dflt);
+  const [{ icon, height, width }, setState] = useState(dflt);
   const classes = useStyles();
 
   return (
@@ -23,8 +21,8 @@ const Skill = ({ name, default: dflt, active }: Props) => {
       alignItems="center">
       <Box
         p={1}
-        onMouseEnter={() => dispatch(displayActive(active))}
-        onMouseLeave={() => dispatch(displayDefault(dflt))}>
+        onMouseEnter={() => setState({ ...active })}
+        onMouseLeave={() => setState({ ...dflt })}>
         <Grid className={classes.iconWrapper} container justify="center" alignItems="center" item>
           <Icon icon={icon} height={height} width={width} />
         </Grid>
