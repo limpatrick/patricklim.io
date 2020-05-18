184d94b7d54a06b08125320f9887c3bf67a76be1
Patrick Lim <patrick.lim012@gmail.com>
2020-05-18 14:01:59 +0200

    add arg parameter to useScrollTo

--- src/hooks/use-scroll-to.ts
+++ src/hooks/use-scroll-to.ts
@@ -1,13 +1,16 @@
 import { useState } from 'react';
 
-const useScrollTo = (idSelector: string) => {
+const useScrollTo = (
+  idSelector: string,
+  arg: Parameters<Element['scrollIntoView']>[0] = { behavior: 'smooth', block: 'center' }
+) => {
   const [id, setId] = useState(idSelector);
 
   const scrollTo = (event: React.MouseEvent & { target: { ownerDocument?: Document } }) => {
     const anchor = (event.target.ownerDocument || document).querySelector(`#${id}`);
 
     if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      anchor.scrollIntoView(arg);
     }
   };
 
