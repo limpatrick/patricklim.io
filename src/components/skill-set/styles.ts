94f9891f7dcb87fde3dddae8ca4d84f0f1939e7b
limpatrick <contact@patricklim.fr>
2020-05-18 23:47:34 +0200

    update root class

--- src/components/skill-set/styles.ts
+++ src/components/skill-set/styles.ts
@@ -1,5 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-  root: { border: '0px solid', minHeight: '100vh' },
+  root: {
+    '& > .MuiGrid-root': { minHeight: '100vh' },
+  },
 });
