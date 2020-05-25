e57b1a10984c0032978604b42212b3f3eb806772
Patrick Lim <patrick.lim012@gmail.com>
2020-05-25 18:42:44 +0200

    update type and reducer

--- src/components/intro/reducer.ts
+++ src/components/intro/reducer.ts
@@ -4,10 +4,9 @@ export const initialState: State = { text1: false, text2: false, text3: false };
 
 export default function reducer(state: State, { type, payload }: Action): State {
   switch (type) {
-    case TEXT_ENTERED: {
+    case TEXT_ENTERED:
       return { ...state, [payload]: true };
-    }
     default:
-      return state;
+      throw new Error(`Unhandled action type: ${type}`);
   }
 }
