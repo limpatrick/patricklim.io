2a5c7450d8b019d9bf84cfdeafaa51055a9e1c6e
limpatrick <contact@patricklim.fr>
2020-05-25 18:42:44 +0200

    update type and reducer

--- src/components/intro/types.ts
+++ src/components/intro/types.ts
@@ -2,10 +2,10 @@ export const TEXT_ENTERED = 'TEXT_ENTERED';
 
 export type Text = keyof State;
 
-export interface TextEnteredAction {
+export type TextEnteredAction = {
   type: typeof TEXT_ENTERED;
   payload: Text;
-}
+};
 
 export type Action = TextEnteredAction;
 export type State = { text1: boolean; text2: boolean; text3: boolean };
