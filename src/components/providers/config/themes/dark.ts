37288d77d6678ae3c32164b50965262fe4a81242
Patrick Lim <patrick.lim012@gmail.com>
2020-05-28 17:20:58 +0200

    add dark theme

--- src/components/providers/config/themes/dark.ts
+++ src/components/providers/config/themes/dark.ts
@@ -1,9 +1,48 @@
-import { createMuiTheme } from '@material-ui/core';
+import { mergeDeepRight } from 'ramda';
+import { createMuiTheme, ThemeOptions } from '@material-ui/core';
+import red from '@material-ui/core/colors/red';
+import teal from '@material-ui/core/colors/teal';
 import base from './base';
 
-export default createMuiTheme({
-  ...base,
-  palette: {
-    type: 'dark',
-  },
-});
+export default createMuiTheme(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  mergeDeepRight<any, ThemeOptions>(base, {
+    palette: {
+      type: 'dark',
+      background: {
+        default: '#212121',
+        paper: '#242424',
+      },
+      primary: {
+        main: '#e0e0e0',
+      },
+      error: {
+        main: red['A100'],
+      },
+      success: {
+        main: teal['A400'],
+      },
+    },
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          '::selection': {
+            background: '#eeeeee',
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        root: {
+          '&:hover $notchedOutline': {
+            borderColor: '#8d8d8d',
+          },
+        },
+      },
+      MuiTypography: {
+        colorPrimary: {
+          color: '#bdbdbd',
+        },
+      },
+    },
+  })
+);
