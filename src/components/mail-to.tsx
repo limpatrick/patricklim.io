ea876664c9d4ff83e9a30a29152e06f96e2501d5
Patrick Lim <patrick.lim012@gmail.com>
2020-05-28 18:13:05 +0200

    add email as a site metadata variable

--- src/components/mail-to.tsx
+++ src/components/mail-to.tsx
@@ -1,12 +1,33 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
+import { ContactEmailQuery } from '@generated/graphql-types';
 import Link from '@material-ui/core/Link';
+
+export const query = graphql`
+  query ContactEmail {
+    site {
+      siteMetadata {
+        email
+        emailSubject
+      }
+    }
+  }
+`;
 
 type Props = { color?: React.ComponentProps<typeof Link>['color'] };
 
-const MailTo = ({ color }: Props) => (
-  <Link color={color ?? 'inherit'} href="mailto:contact@patricklim.fr">
-    contact@patricklim.fr
-  </Link>
-);
+const MailTo = ({ color }: Props) => {
+  const {
+    site: {
+      siteMetadata: { email, emailSubject },
+    },
+  } = useStaticQuery<ContactEmailQuery>(query);
+
+  return (
+    <Link color={color ?? 'inherit'} href={`mailto:${email}?subject=${emailSubject}`}>
+      {email}
+    </Link>
+  );
+};
 
 export default MailTo;
