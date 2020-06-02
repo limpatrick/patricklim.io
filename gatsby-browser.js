8f5bc99bcf6f2d53836742717b5d80ee253ff30e
limpatrick <contact@patricklim.fr>
2020-06-02 14:21:43 +0200

    reset scroll positions on client entry

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,15 @@ import wrapRoot from './src/gatsby/wrap-root-element/browser';
 export const wrapPageElement = wrapPage;
 export const wrapRootElement = wrapRoot;
 
+export const onClientEntry = () => {
+  window.sessionStorage.clear();
+};
+
 export const shouldUpdateScroll = ({ prevRouterProps: { location }, getSavedScrollPosition }) => {
+  const defaultPosition = [0, 0];
   const currentPosition = getSavedScrollPosition(location);
 
-  window.scrollTo(...(currentPosition || [0, 0]));
+  window.scrollTo(...(currentPosition || defaultPosition));
 
   return false;
 };
