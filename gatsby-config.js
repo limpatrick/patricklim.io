e71bd9f37b842b55b772ccfc0a4c9550e993186c
limpatrick <contact@patricklim.fr>
2020-06-06 12:22:30 +0200

    test netlify form submission without sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,14 +51,14 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        workboxConfig: {
-          globPatterns: ['**/*'],
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     workboxConfig: {
+    //       globPatterns: ['**/*'],
+    //     },
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-graphql-codegen`,
       options: {
