617614e7c11cd7d317d3d1666beac581265fd7b0
limpatrick <contact@patricklim.fr>
2020-06-06 14:27:57 +0200

    add spam filter

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { LanguageCode } from '~/typings/global';
 
 export const BACK_TOP_ANCHOR_ID = 'back-to-top-anchor';
+export const FORM_BOT_FIELD = 'bot-field';
 export const FORM_NAME = 'contact';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const LANGUAGES = process.env.LANGUAGES!.split(',') as LanguageCode[];
