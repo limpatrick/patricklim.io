6a0b419fb5fcea97426b6fcbc2554c699f1ab86a
limpatrick <contact@patricklim.fr>
2020-06-06 12:01:58 +0200

    add form name

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import { LanguageCode } from '~/typings/global';
 
+export const BACK_TOP_ANCHOR_ID = 'back-to-top-anchor';
+export const FORM_NAME = 'contact';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export const LANGUAGES = process.env.LANGUAGES!.split(',') as LanguageCode[];
-export const BACK_TOP_ANCHOR_ID = 'back-to-top-anchor';
 export const SKILL_SET_ID = 'skill-set-section';
