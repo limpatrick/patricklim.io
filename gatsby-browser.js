dfb532ec5a9301df0e01e1cf283f2829fea7e9e5
Patrick Lim <patrick.lim012@gmail.com>
2020-06-18 17:05:42 +0200

    add IntersectionObserver polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,12 @@ export const wrapRootElement = wrapRoot;
 
 export const onClientEntry = () => {
   window.sessionStorage.clear();
+
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (!(`IntersectionObserver` in window)) {
+    import(`intersection-observer`);
+    console.log(`# IntersectionObserver is polyfilled!`);
+  }
 };
 
 export const shouldUpdateScroll = ({ prevRouterProps: { location }, getSavedScrollPosition }) => {
