dc38978af63271de524d5145a8b3597a448f2570
limpatrick <contact@patricklim.fr>
2020-06-19 14:21:53 +0200

    fix header's color on 404 page

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -4,7 +4,11 @@ export default makeStyles((theme: Theme) =>
   createStyles({
     default: {
       color: 'rgba(0, 0, 0, 0.54)',
+      '&$defaultDark': {
+        color: 'rgba(255, 255, 255, 0.7)',
+      },
     },
+    defaultDark: {},
     trigger: {
       color: theme.palette.text.secondary,
     },
