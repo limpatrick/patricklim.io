fd3c14c61a8075fd0192a35e1d7c1a798e176679
limpatrick <contact@patricklim.fr>
2020-06-28 20:18:03 +0200

    fix mobile scroll

--- src/components/header/menu-mobile/provider.tsx
+++ src/components/header/menu-mobile/provider.tsx
@@ -1,9 +1,15 @@
+import transitions from '@material-ui/core/styles/transitions';
+import { TransitionHandlerProps } from '@material-ui/core/transitions';
 import React, { createContext, useContext, useEffect, useMemo, useReducer } from 'react';
 import { onCallbackCalled, onExited, setIsOpen } from './actions';
 import reducer from './reducer';
 import { State } from './types';
 
-type Actions = { open: () => void; close: (callback?: () => void) => void; onExited: () => void };
+type Actions = {
+  open: () => void;
+  close: (callback?: () => void) => void;
+  onExited: TransitionHandlerProps['onExited'];
+};
 type Props = { children: React.ReactNode };
 
 const MenuMobileStateContext = createContext<State | undefined>(undefined);
@@ -17,19 +23,28 @@ const MenuMobileProvider = ({ children }: Props) => {
     () => ({
       open: () => dispatch(setIsOpen(true)),
       close: callback => dispatch(setIsOpen(false, callback)),
-      onExited: () => dispatch(onExited()),
+      onExited: node => {
+        const nodeExists0 = document.body.contains(node);
+        alert(`MenuMobileProvider -> nodeExists0=${nodeExists0}`);
+        const id = setInterval(() => {
+          const nodeExists = document.body.contains(node);
+          alert(`id -> nodeExists=${nodeExists}`);
+
+          if (nodeExists === false) {
+            alert(`clearInterval=${id}`);
+            clearInterval(id);
+            dispatch(onExited());
+          }
+        });
+      },
     }),
     []
   );
 
   useEffect(() => {
     if (state.isOpen === false && state.callback) {
-      alert('callback() settimeout');
-
-      setTimeout(() => {
-        if (state.callback) state.callback();
-        dispatch(onCallbackCalled());
-      });
+      state.callback();
+      dispatch(onCallbackCalled());
     }
   }, [state]);
 
