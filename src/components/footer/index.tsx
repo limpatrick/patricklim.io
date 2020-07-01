0a8d24213437a3096eab0b2b1adcfc614b55ebda
limpatrick <contact@patricklim.fr>
2020-07-01 19:02:35 +0200

    add link to github

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import React from 'react';
 import useStyles from './styles';
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
-    <Grid className={classes.root} container justify="center" component="footer">
+    <Grid className={classes.root} container justify="center" component="footer" spacing={1}>
       <Grid item>
         <Typography variant="overline">© {new Date().getUTCFullYear()} Patrick Lim</Typography>
+      </Grid>
+      <Grid item>
+        <IconButton href="https://github.com/limpatrick/patricklim.fr" target="_blank" size="small">
+          <GitHubIcon />
+        </IconButton>
       </Grid>
     </Grid>
   );
